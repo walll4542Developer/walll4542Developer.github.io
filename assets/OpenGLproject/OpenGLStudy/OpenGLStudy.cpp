199043be616e2410e0a7f490b46766a404e489b3
최우영 [wooyoung] <wooyoung@devcat.com>
2024-05-20 15:31:06 +0900

    update

--- assets/OpenGLproject/OpenGLStudy/OpenGLStudy.cpp
+++ assets/OpenGLproject/OpenGLStudy/OpenGLStudy.cpp
@@ -1,5 +1,11 @@
-﻿#include "GLFW/glfw3.h"
+﻿#include"GLEW/glew.h" 
+#include"GLFW/glfw3.h" // include 순서가 glfw3.h 가 glew.h 뒤에 와야 합니다
+
+#pragma comment(lib, "glew32.lib")
+#pragma comment(lib, "opengl32.lib")
 #pragma comment(lib, "glfw3.lib")
+
+#include"stdio.h" 
 
 const unsigned int WIN_W = 300;
 const unsigned int WIN_H = 300;
@@ -11,9 +17,17 @@ int main(void)
     GLFWwindow* window = glfwCreateWindow(WIN_W, WIN_H, "Hello GLFW", NULL, NULL);
     glfwMakeContextCurrent(window);
 
+    // start GLEW
+    glewInit();
+
     // main loop
     while (!glfwWindowShouldClose(window))
     {
+        //draw
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //end loop
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
